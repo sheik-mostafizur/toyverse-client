869d315650dce9a656ba8fa36e12d28dd88bb914
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-05-18 12:34:06 +0600

    change register image

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -8,10 +8,12 @@ const Register = () => {
       <Navbar />
 
       <div className="max-w-screen-xl mx-auto px-4 my-24 grid grid-cols-1 md:grid-cols-2 items-center justify-center gap-4">
-        <img
-          src="https://media.4rgos.it/i/Argos/2621-m007-25-01-8298481-lamborghini-v2"
-          alt=""
-        />
+        <div>
+          <img
+            src="https://img.freepik.com/free-vector/children-repairing-car-together_1308-80884.jpg?w=1380&t=st=1684391046~exp=1684391646~hmac=0f10ee659b08ab772441d257bec59c313a63f20066ba72918eba854ed489f9db"
+            alt=""
+          />
+        </div>
 
         <RegisterForm />
       </div>
